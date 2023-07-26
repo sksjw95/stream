822d45737511b45329efa6b450ec1c513d6b70f4
sksjw95 <tjwldnd95@hanmail.net>
2023-07-26 10:34:41 +0900

    형변환

---
+++ src/stream/Main26.java
@@ -0,0 +1,22 @@
+package stream;
+
+public class Main26 {
+    public static void main(String[] args) {
+        /*(1번)Integer a = 4;
+        int b =  a;
+        System.out.println(b);*/
+       /* (int -> long)
+       int a =3;
+        long b = Long.valueOf(a);
+        System.out.println(b);*/
+        /*(Long->int)
+        Long a = 3L;
+        int b = a.intValue();
+        System.out.println(b);*/
+        /*(long->int)
+        long a = 3;
+        int b = Long.valueOf(a).intValue();
+        System.out.println(b);*/
+
+    }
+}
